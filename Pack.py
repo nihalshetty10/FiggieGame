f8dd789ee6241a5a051ec81a860808eea1cf639f
Nihal Shetty <160050360+nihalshetty10@users.noreply.github.com>
2025-01-03 12:02:55 -0500

    Update Pack.py

--- Pack.py
+++ Pack.py
@@ -4,15 +4,16 @@ from Card import Card
 suits = ["hearts", "diamonds", "clubs", "spades"]
 
 class Pack:
-    def __init__(self, goal_suit, common_suit):
+    def __init__(self, common_suit):
         unused_suits = suits.copy()
 
-        unused_suits.remove(goal_suit)
         unused_suits.remove(common_suit)
+        random_suit = random.choice(unused_suits)
+        unused_suits.remove(random_suit)
 
-        # make 12 common suits, 8 goal suits, 10 (x2) other suit
+        # make 12 common suits, 8 and 10 (x2) other suit
         self.cards = [Card(common_suit) for _ in range(12)] + \
-                     [Card(goal_suit) for _ in range(8)] + \
+                     [Card(random_suit) for _ in range(8)] + \
                      [Card(suit) for suit in unused_suits for _ in range(10)]
         self.shuffle()
 
